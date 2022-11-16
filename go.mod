64bef4845dd962861bc57d27a2660ddb5fa9d511
ty <2805347171@qq.com>
2022-11-16 13:15:36 +0800

    feat: add consul

--- go.mod
+++ go.mod
@@ -5,27 +5,39 @@ go 1.18
 require (
 	github.com/Shopify/sarama v1.34.0
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/google/uuid v1.1.2
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/hashicorp/consul/api v1.12.0
+	github.com/satori/go.uuid v1.2.0
 	github.com/solost23/go_interface v1.0.9
 	github.com/solost23/tools v1.0.2
 	github.com/spf13/viper v1.12.0
+	go.uber.org/zap v1.17.0
 	google.golang.org/grpc v1.47.0
 	gorm.io/gorm v1.23.5
 )
 
 require (
+	github.com/armon/go-metrics v0.3.10 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eapache/go-resiliency v1.2.0 // indirect
 	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
 	github.com/eapache/queue v1.1.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.2.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hashicorp/serf v0.9.7 // indirect
 	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
 	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
 	github.com/jcmturner/gofork v1.0.0 // indirect
@@ -35,6 +47,9 @@ require (
 	github.com/jinzhu/now v1.1.4 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.19.0 // indirect
@@ -48,6 +63,8 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
 	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,10 @@ package server
 
 import (
 	"fmt"
+	"github.com/hashicorp/consul/api"
+	uuid "github.com/satori/go.uuid"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 	"net"
 	"os"
 	"os/signal"
@@ -16,33 +20,67 @@ import (
 	"my_grpc_frame/internal/handler"
 	"my_grpc_frame/internal/interceptor"
 	"my_grpc_frame/internal/models"
+	"my_grpc_frame/pkg/helper"
 )
 
 func Run() {
-	port := viper.GetInt("params.service_port")
-	server, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	if err != nil {
-		panic(err)
-		return
-	}
-	gRPCServer := grpc.NewServer(grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
-		interceptor.Context(),
-		interceptor.Recovery(),
-		interceptor.Logging())))
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			interceptor.Context(),
+			interceptor.Recovery(),
+			interceptor.Logging(),
+		)))
 	// 初始化 handler
-	err = handler.Init(handler.Config{
-		Server:       gRPCServer,
+	err := handler.Init(handler.Config{
+		Server:       server,
 		MysqlConnect: models.NewMysqlConnect(),
 		RedisClient:  models.NewRedisConnect("my_grpc_frame"),
 	})
-	if err != nil {
-		panic(err)
-		return
+	must(err)
+	// 随机获取ip 地址和 未占用端口
+	ip, err := helper.GetInternalIP()
+	must(err)
+	port, err := helper.GetFreePort()
+	must(err)
+
+	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", ip, port))
+	must(err)
+
+	// 注册服务健康检查
+	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+
+	// 服务注册
+	cfg := api.DefaultConfig()
+	cfg.Address = viper.GetString("connections.consul.addr")
+
+	client, err := api.NewClient(cfg)
+	must(err)
+
+	// 生成检查对象
+	check := &api.AgentServiceCheck{
+		Interval:                       "5s",
+		Timeout:                        "5s",
+		GRPC:                           fmt.Sprintf("%s:%d", ip, port),
+		DeregisterCriticalServiceAfter: "10s",
 	}
+
+	// 生成注册对象
+	serviceId := uuid.NewV4().String()
+	registration := new(api.AgentServiceRegistration)
+	registration.Name = viper.GetString("params.service_name")
+	registration.ID = serviceId
+	registration.Port = port
+	registration.Tags = []string{viper.GetString("params.service_name")}
+	registration.Address = ip
+	registration.Check = check
+
+	err = client.Agent().ServiceRegister(registration)
+	must(err)
+
 	// 启动gRPC server
 	go func() {
 		defer RecoverGRoutine("GRpc")
-		err = gRPCServer.Serve(server)
+		err = server.Serve(listen)
 		if err != nil {
 			panic(err)
 		}
@@ -53,7 +91,13 @@ func Run() {
 		si := <-c
 		switch si {
 		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
-			gRPCServer.Stop()
+			// 服务停止
+			server.Stop()
+			// 服务注销
+			if err = client.Agent().ServiceDeregister(serviceId); err != nil {
+				fmt.Println("服务注销失败")
+			}
+			fmt.Println("服务注销成功")
 			// kafka.ConsumerClient.Stop()
 			return
 		case syscall.SIGHUP:
@@ -68,5 +112,11 @@ func RecoverGRoutine(qid interface{}) {
 		buf := make([]byte, 1<<16)
 		runtime.Stack(buf, true)
 		fmt.Println(fmt.Sprintf("[%v] qid:%v, panic:%v\n, runtime:%v", time.Now().Format("2006-01-02 15:04:05"), qid, err, string(buf)))
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
