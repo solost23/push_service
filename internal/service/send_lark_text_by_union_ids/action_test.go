2618eb2638e7d89c876bf8cab4ad12a2604ba6e5
tianyuan <tianyuanyuans@163.com>
2023-01-22 00:04:36 +0800

    feat: send_lark_text_msg test file

--- internal/service/send_lark_text_by_union_ids/action_test.go
+++ internal/service/send_lark_text_by_union_ids/action_test.go
@@ -1,1 +1,82 @@
 package send_lark_text_by_union_ids
+
+import (
+	"context"
+	"fmt"
+	"github.com/solost23/protopb/gen/go/protos/push"
+	"github.com/spf13/viper"
+	"push_service/internal/models"
+	"testing"
+)
+
+var (
+	WebConfigPath = "../../../configs/conf.yml"
+)
+
+func InitConfig() {
+	viper.SetConfigFile(WebConfigPath)
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestAction_Deal(t *testing.T) {
+	InitConfig()
+	mdb := models.NewMysqlConnect()
+	type arg struct {
+		unionIds []string
+		content  string
+	}
+	type want struct {
+		err error
+	}
+	type test struct {
+		name string
+		ctx  context.Context
+		arg  arg
+		want want
+	}
+	tests := []test{
+		{
+			name: "case 1",
+			ctx:  context.Background(),
+			arg: arg{
+				unionIds: []string{"on_8289e3180c41466f18c30f200fdc96e7"},
+				content:  "case 1",
+			},
+			want: want{
+				err: nil,
+			},
+		},
+		{
+			name: "case 2",
+			ctx:  context.Background(),
+			arg: arg{
+				unionIds: []string{"on_8289e3180c41466f18c30f200fdc96e7"},
+				content:  "case 2",
+			},
+			want: want{
+				err: nil,
+			},
+		},
+	}
+	client := NewActionWithCtx(context.Background())
+	client.SetMysql(mdb)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := &push.SendLarkTextByUnionIdsRequest{
+				UnionIds: test.arg.unionIds,
+				Content:  test.arg.content,
+			}
+			_, err := client.Deal(test.ctx, request)
+			if err != test.want.err {
+				t.Error(err)
+				return
+			}
+			fmt.Println("发送飞书文本消息成功")
+		})
+	}
+}
